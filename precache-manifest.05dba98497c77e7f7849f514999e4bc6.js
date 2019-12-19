14afb100deebab77fde977acbe2953caea203620
miri <cindymr91@gmail.com>
2019-12-19 15:38:47 +1100

    Updates

---
+++ precache-manifest.05dba98497c77e7f7849f514999e4bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854c8f230b889ba0d9c45ee24e1c6fb5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "dc38ab23ef0d8bd2936f",
+    "url": "/movie_app_2019/static/css/main.1f5fd726.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc38ab23ef0d8bd2936f",
+    "url": "/movie_app_2019/static/js/main.61b2a325.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
